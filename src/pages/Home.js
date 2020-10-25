0854ddc4b9a15413dc3c7d3b67570b6d46318a7b
zaiba19 <zaibaiqbal1@gmail.com>
2020-10-24 23:20:03 -0400

    added icon to profile

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
-import { GridList, GridListTile, Typography } from '@material-ui/core'
+import { GridList, GridListTile, ListItem, Typography } from '@material-ui/core'
 import React from 'react'
 import ImageGallery from '../components/ImageGallery'
 import Layout from '../components/Layout'
 import SkillList from '../components/SkillList'
 import Zaiba_Iqbal from '../images/Zaiba_Iqbal.jpg';
-
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import EmailIcon from '@material-ui/icons/Email';
 // import eventurous from '../images/eventurous.png'
 import '../components/project.css'
 import AvatarImage from '../components/AvatarImage'
@@ -21,6 +23,11 @@ export default function Home() {
                 <img src={Zaiba_Iqbal}  style={{ width:'15rem', borderRadius:'5px 20px 5px'}} />
                 
                 </p>
+                <a href="https://github.com/Zaiba123"> <GitHubIcon  /></a>
+                <a href="https://www.linkedin.com/in/zaiba-iqbal/"> <LinkedInIcon /></a>
+                {/* <ListItem button key="Email" component="a" href= /> */}
+                <a href="mailto:zaibaiqbal1@gmail.com"> <EmailIcon /></a>
+                
                 <div>
                 <iframe src="Zaiba_Iqbal_Resume.pdf" width = "60%" height = "500px" title="Resume"/>
                 </div>
