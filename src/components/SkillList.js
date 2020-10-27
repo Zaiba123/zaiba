bf850e6db0fe634098f494bbb7c9563685e2a228
zaiba19 <zaibaiqbal1@gmail.com>
2020-10-26 21:25:14 -0400

    changed skills layout

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -4,40 +4,43 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    maxWidth: 752,
-    textAlign: "center",
-    backgroundColor:"lightpink"
-  },
-  demo: {
-    backgroundColor: theme.palette.background.paper,
-    margin:"auto",
-  },
+  // root: {
+  //   flexGrow: 1,
+  //   maxWidth: 752,
+  //   textAlign: "center",
+  //   backgroundColor:"lightpink"
+  // },
+  // demo: {
+  //   backgroundColor: theme.palette.background.paper,
+  //   margin:"auto",
+  // },
   title: {
     margin: theme.spacing(4, 0, 2),
     // maxWidth:"70%",
     textAlign:"center",
   },
-}));
+  skillItems: {
+    display: "flex",
+    flexDirection:"column",
+    flex: 3,
+  }
 
+}));
 
 
 export default function InteractiveList() {
   const classes = useStyles();
-  const languages = ["JavaScript","React.js","C++","Python,SQL","HTML","CSS","Redux","Git","SCSS"]
+  const languages = ["JavaScript","React.js","C++","Python","SQL","HTML","CSS","Redux","Git","SCSS"]
   return (
     <div className={classes.root}>
       <Typography variant="h5" className={classes.title}>
             Skills 
           </Typography>
-        <Grid item xs={12} md={6}>
-          <div className={classes.demo}>
-            <List >
+          <div >
+            <List className={classes.skillItems}>
                 {
                   languages.map(lan => {
                       return (
@@ -51,11 +54,9 @@ export default function InteractiveList() {
                       </ListItem>
                       )
                   })
-                }   
+                }
             </List>
           </div>
-        </Grid>
-       
 
     </div>
   );
