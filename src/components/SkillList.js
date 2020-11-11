b66d463ff242b29d763181c629f7ed2f339a7072
zaiba19 <zaibaiqbal1@gmail.com>
2020-11-11 18:56:33 -0500

    aligned skills items

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -1,51 +1,29 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import ListItemText from '@material-ui/core/ListItemText';
+
 import Typography from '@material-ui/core/Typography';
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
-const useStyles = makeStyles((theme) => ({
-  title: {
-    margin: theme.spacing(4, 0, 2),
-    // maxWidth:"70%",
-    textAlign:"center",
- 
-  },
-  skillList: {
-    display: "grid",
-    // flexDirection:'row',
-    gridTemplateColumns:"1fr 1fr 1fr",
-    // flexWrap: "wrap",
-  },
-  skillName: {
-    // display: "flex",
-    marginLeft:"20px",
-    marginRight:"20px",
-    margin: "10px",
-    textAlign: "center",
-    fontSize: "30px",
-  }
+import '../styles/skillList.css'
 
-}));
 
 
 export default function InteractiveList() {
-  const classes = useStyles();
+  // const classes = useStyles();
   const languages = ["JavaScript","React.js","C++","Python","SQL","HTML","CSS","Redux","Git","SCSS"]
   return (
-    <div className={classes.root}>
-      <Typography variant="h5" className={classes.title}>
+    <div >
+      <Typography variant="h5" >
             Skills 
           </Typography>
-            <div classes={classes.skillList}>
+            <div className="f-box">
                 {
                   languages.map(lan => {
                       return (
-                        <div classes={classes.skillName}>
-                          <WbSunnyIcon />
-                        <ListItemText 
-                          primary={lan}
-                        />
-                      </div>
+                        
+                          <div className="flex-container">
+                            <WbSunnyIcon />
+                            {lan}
+                          </div>
+                        
                       )
                   })
                 }
