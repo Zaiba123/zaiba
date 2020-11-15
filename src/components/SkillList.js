b43e602b4c7247f6a0fa86c01ce9a5c123095c1e
zaiba19 <zaibaiqbal1@gmail.com>
2020-11-14 23:57:10 -0500

    took sun emoji out

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import Typography from '@material-ui/core/Typography';
-import WbSunnyIcon from '@material-ui/icons/WbSunny';
 import '../styles/skillList.css'
 
 
 
 export default function InteractiveList() {
-  // const classes = useStyles();
   const languages = ["JavaScript","React.js","C++","Python","SQL","HTML","CSS","Redux","Git","SCSS"]
   return (
     <div >
@@ -18,12 +16,9 @@ export default function InteractiveList() {
                 {
                   languages.map(lan => {
                       return (
-                        
                           <div className="flex-container">
-                            <WbSunnyIcon />
                             {lan}
                           </div>
-                        
                       )
                   })
                 }
