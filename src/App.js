86c3dd2b6d8525c8097ead0a8dc270fb659bfa02
zaiba19 <zaibaiqbal1@gmail.com>
2020-11-19 12:59:46 -0500

    fixed imports

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
 import './App.css';
 import Home from './pages/Home';
-import Goals from './pages/Goals';
-import Projects from './pages/Projects';
 import Navigation from './Layout/Navigation';
 
 function App() {
   return (
-  //   <Router>
-  //   <div>
-  //   <Switch>
-  //       <Route exact path="/">
-  //         <Home />
-  //       </Route>
-  //       <Route path="/project">
-  //           <Projects />
-  //       </Route>
-  //       <Route path="/goals">
-  //         <Goals />
-  //       </Route>
-  //   </Switch>
-  // </div>
-  // </Router>
   <div>
     <Navigation style={{position:"fixed", zIndex:100}}/>
     <Home/>
