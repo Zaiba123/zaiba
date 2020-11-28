26876b9e41d1a409348f678fb67eb5fd8e15b494
zaiba19 <zaibaiqbal1@gmail.com>
2020-11-27 22:34:01 -0500

    updating

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -42,10 +42,10 @@ export default function Home() {
                         <b>ABOUT ME</b>
                          <br/> 
                          {/* Can do this with flex column  */}
-                        Hi my Name is Zaiba, I have a passion for solving problems in creative ways as well as creating things. 
+                        Hi my name is Zaiba, I have a passion for solving problems in creative ways as well as creating things. 
                         In my spare time I love learning new technolgies as well as improving on ones I'm currently working on. Besides coding 
-                        I love crocheting and making just about anything I find online. I like being able to put my
-                        creatvity into reality. To relax I love exploring the city and finding new places to eat and blog them on my page.
+                        I love crocheting by making just about anything I find online. I like being able to put my
+                        creatvity into reality. To relax I love exploring the city and finding new places to eat and blog them on my instagram page. A fun fact about me is that I can solve a rubik cube in less than 30 seconds.
                         <br/>
                         <b> Here are some of my technical skills: </b>
                         <SkillList className="skill-list"/>
