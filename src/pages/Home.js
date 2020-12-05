c9a3247bb01e4ff4ee95e429c5990f7c3720ef51
zaiba19 <zaibaiqbal1@gmail.com>
2020-12-04 22:03:50 -0500

    making site mobile compatible

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,11 @@ import React from 'react'
 import SkillList from '../components/SkillList'
 import Zaiba_Iqbal from '../images/Zaiba_Iqbal.jpg';
 import sunflower from '../images/sunflower.jpeg';
+import resume from '../images/resume.png';
 import instagramLogo from '../images/instagramLogo.png';
+import linkedin from '../images/linkedin.png';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@material-ui/icons/Email';
-import DescriptionIcon from '@material-ui/icons/Description';
 import '../components/project.css'
 import '../styles/landingPage.css'
 import '../styles/bio.css'
@@ -23,14 +23,13 @@ export default function Home() {
                         <div className=" centered1"><b>Zaiba Iqbal </b></div>
                         <div className="logo-header centered"><b> Front-End Engineer</b></div>
                     </div>
-                    {/* <div class="centered">Zaiba Iqbal | Front-End Engineer</div> */}
                 </div>
                 <div className="sticky2">
-                    <a href="https://github.com/Zaiba123" className="media-link"> <GitHubIcon fontSize="large" style={{ color:'black'}} /></a>
-                    <a href="https://www.linkedin.com/in/zaiba-iqbal/" className="media-link"> <LinkedInIcon fontSize="large" style={{ color:'#0072b1'}}/></a>
-                    <a href="mailto:zaibaiqbal1@gmail.com" className="media-link"> <EmailIcon  fontSize="large" style={{ color:'black'}}/></a>
-                    <a href="https://www.instagram.com/zfoodgal/" className="media-link"> <img alt="instagram" src={instagramLogo } style={{ width:'29px'}} /></a>
-                    <a href="Zaiba_Iqbal_Resume.pdf" download title="Resume" className="media-link"><DescriptionIcon fontSize="large" style={{ color:'black'}}/></a> 
+                    <a href="https://github.com/Zaiba123" className="media-link"> <GitHubIcon fontSize="large" style={{ color:'black',marginTop:".5rem"}} /></a>
+                    <a href="https://www.linkedin.com/in/zaiba-iqbal/" className="media-link"> <img alt="sunflower" src={linkedin } style={{ width:'35px',marginTop:".5rem"}}/></a>
+                    <a href="mailto:zaibaiqbal1@gmail.com" className="media-link"> <EmailIcon  fontSize="large" style={{ color:'black',marginTop:".5rem"}}/></a>
+                    <a href="https://www.instagram.com/zfoodgal/" className="media-link"> <img alt="instagram" src={instagramLogo } style={{ width:'35px',marginTop:".5rem"}} /></a>
+                    <a href="Zaiba_Iqbal_Resume.pdf" download title="Resume" className="media-link"><img alt="sunflower" src={resume } style={{ width:'35px',marginTop:".5rem"}}/></a> 
                 </div>
 
            
