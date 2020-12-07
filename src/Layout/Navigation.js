a6bdfc157eb09f03b24497ad627b3d03fe7b4429
zaiba19 <zaibaiqbal1@gmail.com>
2020-12-06 21:49:02 -0500

    fixing intro in mobile

--- src/Layout/Navigation.js
+++ src/Layout/Navigation.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from "react-scroll"
 import HomeIcon from '@material-ui/icons/Home';
 import ComputerIcon from '@material-ui/icons/Computer';
-import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import education from '../images/education.jpg';
 
 import '../styles/navigation.css';
 
@@ -17,7 +17,7 @@ const Navigation =()=> {
             smooth={true}
             offset={-70}
             duration={500}>
-          <span role="img" aria-label="home"><HomeIcon fontSize="large"/> </span></Link>
+          <span role="img" aria-label="home"><HomeIcon style={{ width:'35px',height:"35px"}}/> </span></Link>
           <Link
             className='navlink'
             activeClass='active'
@@ -26,7 +26,7 @@ const Navigation =()=> {
             smooth={true}
             offset={-70}
             duration={500}>
-          <span role="img" aria-label="projects"><ComputerIcon fontSize="large" /></span></Link>
+          <span role="img" aria-label="projects"><ComputerIcon style={{ width:'35px',height:"35px"}} /></span></Link>
           <Link
             className='navlink'
             activeClass='active'
@@ -35,7 +35,7 @@ const Navigation =()=> {
             smooth={true}
             offset={-70}
             duration={500}>
-          <span role="img" aria-label="aboutme"><EmojiPeopleIcon fontSize="large"/></span></Link>
+          <span role="img" aria-label="aboutme"><img alt="sunflower" src={education } style={{ width:'39px',height:"35px"}}/></span></Link>
         </nav>
     )
 }
