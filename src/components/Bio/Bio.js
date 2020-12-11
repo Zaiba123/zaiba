b8f278e553916e218dbdfb4867199b9e2c99ec7d
zaiba19 <zaibaiqbal1@gmail.com>
2020-12-10 22:11:34 -0500

    made project text left aligned

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -9,14 +9,13 @@ const Bio = () => {
         <div className="bio-container">
             <img alt="Profile" src={Zaiba_Iqbal}  className="profile"  />
             <div className="intro">
-                <b>ABOUT ME</b>
-                <br/> 
+                <div className="aboutme-title">ABOUT ME</div>
                 Hi my name is Zaiba, I have a passion for solving problems in creative ways as well as creating things. 
                 In my spare time I love learning new technologies as well as improving on ones I'm currently working on. Besides coding 
                 I love crocheting by making just about anything I find online. I like being able to put my
                 creatvity into reality. To relax I love exploring the city and finding new places to eat and blog them on my instagram page. A fun fact about me is that I can solve a rubik cube in less than 30 seconds.
                 <br/>
-                <b> Here are some of my technical skills: </b>
+                <div className="aboutme-title"> Here are some of my technical skills: </div>
                 <SkillList className="skill-list"/>
             </div>
         </div>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -4,16 +4,14 @@ import '../components/WorkAndProjects/workAndProjects.css'
 const Cards = ({name,description,img,linkImage,imageTitle,linkToProject,linkText}) => {
     return (
         <div className="all-cards">
-            {
-                <div className="card-project">
-                    <img src={img} alt="Avatar" className="image-project"/>
-                    <div className="container-project">
-                        <h4><b><a href={linkToProject} > {name} </a></b></h4> 
-                        <p>{description}</p> 
-                        <a href={linkToProject} > {linkImage}  </a>   
-                    </div>
+            <div className="card-project">
+                <img src={img} alt="Avatar" className="image-project"/>
+                <div className="container-project">
+                    <h4><b><a href={linkToProject} > {name} </a></b></h4> 
+                    <p className="project-text">{description}</p> 
+                    <a href={linkToProject} > {linkImage}  </a>   
                 </div>
-            }
+            </div>
         </div>
     )
 }
