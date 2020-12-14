a69ea716932891d8ae49e0a27cec1881886538cd
zaiba19 <zaibaiqbal1@gmail.com>
2020-12-13 22:21:29 -0500

    added to intro

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -10,7 +10,7 @@ const Bio = () => {
             <img alt="Profile" src={Zaiba_Iqbal}  className="profile"  />
             <div className="intro">
                 <div className="aboutme-title">ABOUT ME</div>
-                Hi my name is Zaiba, I have a passion for solving problems in creative ways as well as creating things. 
+                Hi my name is Zaiba, I love solving problems in creative ways as well as creating things. 
                 In my spare time I love learning new technologies as well as improving on ones I'm currently working on. Besides coding 
                 I love crocheting by making just about anything I find online. I like being able to put my
                 creatvity into reality. To relax I love exploring the city and finding new places to eat and blog them on my instagram page. A fun fact about me is that I can solve a rubik cube in less than 30 seconds.
