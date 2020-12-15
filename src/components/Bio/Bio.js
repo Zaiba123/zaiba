babc8a046f0f6c3bcdfcf11e1d756061ea3d04ba
zaiba19 <zaibaiqbal1@gmail.com>
2020-12-14 22:42:56 -0500

    added to intro

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -12,7 +12,7 @@ const Bio = () => {
                 <div className="aboutme-title">ABOUT ME</div>
                 Hi my name is Zaiba, I love solving problems in creative ways as well as creating things. 
                 In my spare time I love learning new technologies as well as improving on ones I'm currently working on. Besides coding 
-                I love crocheting by making just about anything I find online. I like being able to put my
+                I love crocheting by making just about anything I find online such as scarfs, sweaters or stuffed animals. I like being able to put my
                 creatvity into reality. To relax I love exploring the city and finding new places to eat and blog them on my instagram page. A fun fact about me is that I can solve a rubik cube in less than 30 seconds.
                 <br/>
                 <div className="aboutme-title"> Here are some of my technical skills: </div>
