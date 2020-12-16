712aa9e79dfe206e7a384384fed2322a50b7694c
zaiba19 <zaibaiqbal1@gmail.com>
2020-12-15 22:06:20 -0500

    added neon to bio title

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -9,13 +9,13 @@ const Bio = () => {
         <div className="bio-container">
             <img alt="Profile" src={Zaiba_Iqbal}  className="profile"  />
             <div className="intro">
-                <div className="aboutme-title">ABOUT ME</div>
+                <div className="aboutme-title"><b>A<span>b</span>ou<span>t</span> M<span>e</span></b></div>
                 Hi my name is Zaiba, I love solving problems in creative ways as well as creating things. 
                 In my spare time I love learning new technologies as well as improving on ones I'm currently working on. Besides coding 
                 I love crocheting by making just about anything I find online such as scarfs, sweaters or stuffed animals. I like being able to put my
                 creatvity into reality. To relax I love exploring the city and finding new places to eat and blog them on my instagram page. A fun fact about me is that I can solve a rubik cube in less than 30 seconds.
                 <br/>
-                <div className="aboutme-title"> Here are some of my technical skills: </div>
+                <div className="aboutme-technicalskills "> Here are some of my technical skills: </div>
                 <SkillList className="skill-list"/>
             </div>
         </div>
