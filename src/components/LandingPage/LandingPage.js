609f29e29dcad08ee125a770c3018a787b2b38f3
zaiba19 <zaibaiqbal1@gmail.com>
2020-12-16 19:17:32 -0500

    took away space

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -7,7 +7,7 @@ import '../../components/LandingPage/landingPage.css'
 const LandingPage = () =>{
     return (
         <div className="container-flower">
-            <img alt="sunflower" src={sunflower } className="landing-page"  />
+            <img alt="sunflower" src={sunflower} className="landing-page"  />
             <div className= "l-page-group">
                 <div className=" centered1"><b>Zaiba Iqbal </b></div>
                 <div className="logo-header centered"><b> Front-End Engineer</b></div>
