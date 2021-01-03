499fdb4284b3bfa7f41287d73e92d8d2513de36f
zaiba19 <zaibaiqbal1@gmail.com>
2021-01-02 22:27:54 -0500

    fixed spacing

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -13,7 +13,7 @@ const Bio = () => {
                 Hi my name is Zaiba, I love solving problems in creative ways as well as creating things. 
                 In my spare time I love learning new technologies as well as improving on ones I'm currently working on. Besides coding 
                 I love crocheting by making just about anything I find online such as scarfs, sweaters or stuffed animals. I like being able to put my
-                creatvity into reality. In my spare time I love exploring the city and finding new places to eat and blog them on my instagram page. A fun fact about me is that I can solve a rubik cube in less than 30 seconds.
+                creatvity into reality. In my spare time I love exploring the city and finding new places to eat and blog about them on my instagram page. A fun fact about me is that I can solve a rubik cube in less than 30 seconds.
                 <br/>
                 <div className="aboutme-technicalskills "> Here are some of my technical skills: </div>
                 <SkillList className="skill-list"/>
